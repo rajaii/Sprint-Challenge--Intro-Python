88be87fab35c19f3521d580fcff18ade8b5cdd81
Ali Rajaii <rajaii_a@yahoo.com>
2020-01-24 23:59:01 -0500

    cities passes

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,12 +1,20 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon  
+      
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
 # In the body of the `cityreader` function, use Python's built-in "csv" module 
 # to read this file so that each record is imported into a City instance. Then
+
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
 #
@@ -14,13 +22,33 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
+csv.reader('cities.csv') 
+with open('cities.csv', newline='') as csvfile:
+    cityreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for c in cityreader:
+        print(c[0].split(','))
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    csv.reader('cities.csv') 
+    with open('cities.csv', newline='') as csvfile:
+        cityreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for c in cityreader:
+            # spl = c[0].split(',')
+            if len(c) > 3 and not c[0]:
+            #cities.append(City(c[0], c[3],  c[4]))
+                
+            # print(spl[0][3])
+            # print(spl[3])
+            # print(spl[4])
+                cities.append(City(c[0], float(c[3]),  float(c[4])))
+
     return cities
 
 cityreader(cities)
